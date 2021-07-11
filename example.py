1c8dbb1eb45a6f398682714a7b86d8b9bea3caf8
prathamesh patkar <2000prath@gmail.com>
2021-07-11 14:26:47 +0530

    newly generated images

--- example.py
+++ example.py
@@ -4,13 +4,14 @@ from avatar import AvatarGenerator
 obj = AvatarGenerator()
 
 #with default parameters
-img = obj.CreateAvatar() 
-img.save('With-default.png')
+for i in range(20,35):
+    img = obj.CreateAvatar() 
+    img.save('images/sample'+str(i)+'.png')
 
-#with parameters and with border
-img = obj.CreateAvatar(xy_axis=5,pixels=300,background_color='white',border=True,border_width=25) 
-img.save('Advance-Parameters-border.png')
+# #with parameters and with border
+# img = obj.CreateAvatar(xy_axis=5,pixels=300,background_color='white',border=True,border_width=25) 
+# img.save('Advance-Parameters-border.png')
 
-#with parameters and no border
-img = obj.CreateAvatar(xy_axis=5,pixels=300,background_color='white',border=False) 
-img.save('Advance-Parameters-noborder.png')
+# #with parameters and no border
+# img = obj.CreateAvatar(xy_axis=5,pixels=300,background_color='white',border=False) 
+# img.save('Advance-Parameters-noborder.png')
